36d982392c3db2aeb4cf354f5dba7d4e0f4cd1ed
Jonny-exe <69385502+Jonny-exe@users.noreply.github.com>
2020-08-29 23:44:58 +0200

    Removed hard coded color values

--- keyboard.js
+++ keyboard.js
@@ -1,7 +1,9 @@
+//
+// HTML-keyboard/keyboard.js
+//
 
 document.body.addEventListener('keydown', function(event) {
   var keynum;
-  console.log("onkeydown");
   if (window.event) { // IE
     keynum = event.keyCode;
   } else if (event.which) { // Netscape/Firefox/Opera
@@ -10,16 +12,21 @@ document.body.addEventListener('keydown', function(event) {
   //use something like charCodeAt to get the number.
   pressedKey = String.fromCharCode(keynum);
   pressedKey = pressedKey.charCodeAt();
-
   return changeKeyColor(pressedKey);
 });
 
 function changeKeyColor(lastKey) {
-  //change this to change the color that appears when you press a key
-  document.getElementById(lastKey).style.backgroundColor = "#828282";
+  var keyelement = document.getElementById(lastKey);
+  var keystyle = getComputedStyle(keyelement);
+  var keyselectionstyle = getComputedStyle(keyelement, '::selection');
+  var backColor = keystyle.backgroundColor;  //the RGB value for background: e.g. rgb(12,34,56)
+  // console.log(backColor);
+  var blinkColor = keyselectionstyle.backgroundColor; //the RGB value for blink: e.g. rgb(12,34,56)
+  //console.log(blinkColor);
+
+  document.getElementById(lastKey).style.backgroundColor = blinkColor; // blink
 
   setTimeout(function() {
-    //this color should be the same as the CSS background color
-    document.getElementById(lastKey).style.backgroundColor = "#aaaaaa";
+    document.getElementById(lastKey).style.backgroundColor = backColor; // default, std. bg
   }, 100);
 }
